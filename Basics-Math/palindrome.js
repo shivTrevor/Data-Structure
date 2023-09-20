f298db5c8c0d2085a3540e86e46567bd0430753a
Shivnandan Tiwari <36485361+shivTrevor@users.noreply.github.com>
2023-09-20 11:18:12 +0530

    Palindrome
    Here in this code we are checking whether the code is palindrome or not by both iterative and recursive approach

--- Basics-Math/palindrome.js
+++ Basics-Math/palindrome.js
@@ -1,8 +1,46 @@
 //Here we are finding whether the number is palindrome or not 
 
-function Palindrome (num){
-    //converting the javascript number to the array 
-    
+/*
+  Algorithm-->
+  1> Reversing the number 
+  2> if the reversed number is equal to the previous number then its palindrome
+  3> else is not a palindrome 
+*/
 
+function Palindrome (num){
+     let rev=0,rem=0;
+
+    //Here reversing a number 
+    while(num){
+   rem=num%10;
+   rev=rev*10+rem;
+   num = Math.floor(num/10);
+    }
+    return rev;
 
 }
+
+// Calling function iteratively
+function recPal(num,rev){
+  if(num==0){return rev}
+  else{
+    let rem = num%10;
+    rev=rev*10+rem;
+    num = Math.floor(num/10)
+    console.log(num)
+    return recPal(num,rev)
+  }
+}
+
+
+  //caliing the recursive palindrome function 
+  let numb =123321
+  let isRev= recPal(numb,0);//Recursive calling 
+  // Here we can call by the iterative method
+  console.log(isRev)
+  if(isRev==numb){console.log("The number is palindrome")}
+  else{console.log("The number is not palindrome")}
+
+
+
+
