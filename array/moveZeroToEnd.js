a9253b61d5e51fc696c711b891657584c998730a
Shivnandan Tiwari <36485361+shivTrevor@users.noreply.github.com>
2023-10-31 16:12:53 +0530

    Update moveZeroToEnd.js
    with space and time => n complexity

--- array/moveZeroToEnd.js
+++ array/moveZeroToEnd.js
@@ -32,6 +32,39 @@ function moveZeroesToEnd(arr){
 }
 
 
+// let arr=[1,2,0,4,3,5,0,8,4,3,0,1];
+//  moveZeroesToEnd(arr);
+// console.log(arr)
+
+
+//=======================================================
+//By using space complexity -> O(n) and time complexity -> O(n)
+
+function zeroesToEnd(){
+   let result=[];
+   let count=0;
+
+  //wherever zeroes comes skip that part  and if not zero push it in result array 
+  for(let i=0;i<arr.length;i++){
+
+    if(arr[i]!==0){
+      result.push(arr[i]);
+    }
+    else{
+      //count zeroes
+      count++;
+    }
+  }
+
+  //push the remaining elements with zeroes bro 
+  for(let i=1;i<=count;i++){
+    result.push(0);
+  }
+
+  return result ;
+   
+}
+
 let arr=[1,2,0,4,3,5,0,8,4,3,0,1];
- moveZeroesToEnd(arr);
-console.log(arr)
+let zerosArray = zeroesToEnd(arr)
+console.log(zerosArray)
