ec73148b3c7929e2ae89850ee3304fc88bd25d58
Shivnandan Tiwari <36485361+shivTrevor@users.noreply.github.com>
2023-11-09 11:56:29 +0530

    Create productOfBigNumbers.js
    with using built in ds

---
+++ Basics-Math/productOfBigNumbers.js
@@ -0,0 +1,10 @@
+//By using the built in library
+const num1 = BigInt('123456789012345678901234567890');
+const num2 = BigInt('987654321098765432109876543210');
+
+const product = num1 * num2;
+
+console.log(product.toString());
+
+
+//without built in ds
