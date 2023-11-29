a31bbfce706576413f60930b3c0aeae9dcc9347a
Shivnandan Tiwari <36485361+shivTrevor@users.noreply.github.com>
2023-11-29 14:59:11 +0530

    Update substrMethod.js
    Recursion

--- basic-recursion/substrMethod.js
+++ basic-recursion/substrMethod.js
@@ -1,6 +1,27 @@
-//so here i am working in the javscript substr method
-//general introductory js substr method
-//so here finding one more
-
-
-
+function printSubset(input, output, index) {
+    // If all elements have been traversed
+    if (index >= input.length) {
+      // Print the output array
+      console.log(output);
+      return;
+    }
+  
+    // Do not include the current element
+    printSubset(input, output, index + 1);
+  
+    // Include the current element
+    output.push(input[index]);
+  
+    printSubset(input, output, index + 1);
+  
+    // Backtrack to remove the current element from the output array
+    output.pop();
+  }
+  
+  // Example usage:
+  const inputArray = [1, 2, 3];
+  const outputArray = [];
+  const startIndex = 0;
+  
+  printSubset(inputArray, outputArray, startIndex);
+  
